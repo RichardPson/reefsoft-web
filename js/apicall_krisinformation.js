baafdf5d7a4a99997c34fffce273c0ff94502693
Richard Pettersson <76970970+RichardPson@users.noreply.github.com>
2024-12-14 22:24:43 +0100

    Testing

--- js/apicall_krisinformation.js
+++ js/apicall_krisinformation.js
@@ -1,7 +1,5 @@
 async function getfromKrisinformation() {
-	const request = new Request(
-		"https://api.krisinformation.se/v3/notifications"
-	);
+	const request = new Request("https://api.krisinformation.se/v3/news");
 	const response = await fetch(request);
 	const news = await response.json();
 	console.log(news);
