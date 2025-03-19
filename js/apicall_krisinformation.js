7c382bd7d0b7c3c977b320710384ba48e9073c94
Richard Pettersson <76970970+RichardPson@users.noreply.github.com>
2025-03-19 09:50:45 +0100

    updated api call string to 30 days history

--- js/apicall_krisinformation.js
+++ js/apicall_krisinformation.js
@@ -1,5 +1,7 @@
 async function getfromKrisinformation() {
-	const request = new Request("https://api.krisinformation.se/v3/testvmas");
+	const request = new Request(
+		"https://api.krisinformation.se/v3/news?allCounties=true&days=30"
+	);
 	const response = await fetch(request);
 	const news = await response.json();
 	console.log(news);
