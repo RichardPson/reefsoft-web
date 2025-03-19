9e93dc9b12c09e94261fc0a25f22d73b38dc9fc0
Richard Pettersson <76970970+RichardPson@users.noreply.github.com>
2025-03-19 09:58:12 +0100

    Update api_calls

--- js/apicall_krisinformation.js
+++ js/apicall_krisinformation.js
@@ -1,6 +1,6 @@
 async function getfromKrisinformation() {
 	const request = new Request(
-		"https://api.krisinformation.se/v3/news?allCounties=true&days=30"
+		"https://api.krisinformation.se/v3/news?allCounties=true&days=60"
 	);
 	const response = await fetch(request);
 	const news = await response.json();
